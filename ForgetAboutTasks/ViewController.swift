b8df2aab069f77de85abb77e15c1fbaf5319fddb
Константин Малков <kmalkov_1995@mail.ru>
2023-03-06 11:19:18 +0300

    Initial Commit

---
+++ ForgetAboutTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForgetAboutTasks
+//
+//  Created by Константин Малков on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
