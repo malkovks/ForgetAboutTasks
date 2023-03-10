1647b6c2ca442d5b988325217e0a8fbe6ddee013
Константин Малков <kmalkov_1995@mail.ru>
2023-03-10 11:28:20 +0300

    Imported FSCalendar and created two view -setting up Schedule VC and Tasks VC -testing to use NSConstraint

--- ForgetAboutTasks/Controllers/ScheduleViewController.swift
+++ ForgetAboutTasks/Controllers/ScheduleViewController.swift
@@ -3,24 +3,115 @@
 //  ForgetAboutTasks
 //
 //  Created by Константин Малков on 09.03.2023.
-//
+/*
+ class with displaying calendar and some events
+ */
 
 import UIKit
-
-import UIKit
+import FSCalendar
 
 class ScheduleViewController: UIViewController {
+    
+    private var calendar: FSCalendar = {
+       let calendar = FSCalendar()
+        calendar.scrollDirection = .vertical
+        calendar.pagingEnabled = false
+        calendar.weekdayHeight = 30
+        calendar.headerHeight = 50
+        calendar.locale = Locale(identifier: "ru_RU")
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
+    private let hideButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Open Calendar", for: .normal)
+        button.setTitleColor(#colorLiteral(red: 0.04713427275, green: 0.08930709213, blue: 0.1346856952, alpha: 1), for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir Next Demi Bold", size: 16)
+        button.backgroundColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+        button.layer.cornerRadius = 12
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupNavigationController()
 
-        view.backgroundColor = .systemBackground
-        title = "Schedule"
-        navigationController?.navigationItem.largeTitleDisplayMode = .always
-        navigationController?.navigationBar.prefersLargeTitles = true
         
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        calendar.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+    }
+    
+    @objc private func didTapTapped(){
+        if !calendar.pagingEnabled {
+            calendar.pagingEnabled = true
+            navigationItem.leftBarButtonItem?.image = UIImage(systemName: "eye.slash.fill")
+        } else {
+            calendar.pagingEnabled = false
+            navigationItem.leftBarButtonItem?.image = UIImage(systemName: "eye.fill")
+        }
+    }
+    
 
+    
+    private func setupView(){
+        setupTarget()
+        view.addSubview(calendar)
+        calendar.delegate = self
+        calendar.dataSource = self
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupTarget(){
+        
+    }
+    
+    private func setupNavigationController(){
+        title = "Schedule"
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "eye.fill"), landscapeImagePhone: nil, style: .done, target: self, action: #selector(didTapTapped))
+    }
+    
 
+    
 }
+//MARK: - calendar delegates
+extension ScheduleViewController: FSCalendarDelegate, FSCalendarDataSource {
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendar.frame.size.height = bounds.height
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+    
+}
+
+
+//    private func setupSwipeAction(){
+//        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(didTapSwipe))
+//        swipeUp.direction = .up
+//        calendar.addGestureRecognizer(swipeUp)
+//
+//        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(didTapSwipe))
+//        swipeDown.direction = .up
+//        calendar.addGestureRecognizer(swipeDown)
+//    }
+    
+//    @objc private func didTapSwipe(gesture: UISwipeGestureRecognizer){
+//        switch gesture.direction {
+//        case .up:
+//            didTapTapped()
+//        case .down:
+//            didTapTapped()
+//        default:
+//            break
+//        }
+//    }
+
--- ForgetAboutTasks/Controllers/TasksViewController.swift
+++ ForgetAboutTasks/Controllers/TasksViewController.swift
@@ -6,17 +6,122 @@
 //
 
 import UIKit
+import FSCalendar
 
 class TasksViewController: UIViewController {
     
+    private var calendarHeightConstraint: NSLayoutConstraint!
+    
+    private var calendar: FSCalendar = {
+       let calendar = FSCalendar()
+        calendar.scrollDirection = .vertical
+        calendar.scope = .week
+        calendar.locale = Locale(identifier: "ru_RU")
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
+    private let tableView: UITableView = {
+       let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .secondarySystemBackground
-        title = "Tasks"
-        navigationController?.navigationItem.largeTitleDisplayMode = .always
-        navigationController?.navigationBar.prefersLargeTitles = true
+        setupView()
+        setupNavigationController()
+        setupTableView()
+        setupConstraintsForCalendar()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    @objc private func didTapTapped(){
+        if calendar.scope == .month {
+            calendar.setScope(.week, animated: true)
+            navigationItem.leftBarButtonItem?.image = UIImage(systemName: "eye.fill")
+        } else {
+            calendar.setScope(.month, animated: true)
+            navigationItem.leftBarButtonItem?.image = UIImage(systemName: "eye.slash.fill")
+        }
+    }
+    
+    private func setupView(){
+        setupTarget()
+        view.addSubview(calendar)
+        calendar.delegate = self
+        calendar.dataSource = self
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self
+                           , forCellReuseIdentifier: "cell")
+    }
+    
+    private func setupTarget(){
         
     }
+    
+    private func setupNavigationController(){
+        title = "Schedule"
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "eye.slash.fill"), landscapeImagePhone: nil, style: .done, target: self, action: #selector(didTapTapped))
+    }
+    
 
+    
+}
+//MARK: - table delegates and datasource
+extension TasksViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = "Check label"
+        cell.detailTextLabel?.text = "Detail label"
+        cell.imageView?.image = UIImage(systemName: "car")
+        return cell
+    }
+}
+
+//MARK: - calendar delegates
+extension TasksViewController: FSCalendarDelegate, FSCalendarDataSource {
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint?.constant = bounds.height
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+}
+//constrain extension for dymanic height changing
+extension TasksViewController {
+    func setupConstraintsForCalendar(){
+        view.addSubview(calendar)
+        calendarHeightConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeightConstraint)
+        
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
+            calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 0),
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: 0)
+        ])
+        
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: calendar.bottomAnchor,constant: 10),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor , constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
 }
